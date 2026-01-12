8c942887a5eba0a4ae908907013adeea268e4d01
Quirijn <quirijn@mail.org>
2026-01-12 01:10:14 +0100

    added watch mode

--- install.sh
+++ install.sh
@@ -1,21 +1,55 @@
 #!/bin/sh
 
 CONFIG_DIR="$HOME/.config"
+WATCH_MODE=0
+
+# Enable watch mode if --watch is passed
+if [ "$1" = "--watch" ]; then
+  WATCH_MODE=1
+fi
 
 sync_dir() {
-    src="$1"
-    dest="$CONFIG_DIR/$1"
+  src="$1"
+  dest="$CONFIG_DIR/$1"
 
-    rsync -a --itemize-changes "$src/" "$dest/" \
-        | awk '/^[><]f/ {
-            print $2 " has been modified!"
-        }'
+  # Capture rsync output
+  changes=$(rsync -a --itemize-changes "$src/" "$dest/" \
+    | awk '/^[><]f/ { print $2 }')
+
+  if [ -n "$changes" ]; then
+    echo "$changes" | while read -r file; do
+      echo "$file has been modified!"
+    done
+    return 0
+  else
+    return 1
+  fi
 }
 
-sync_dir hypr
-sync_dir waybar
-sync_dir wofi
-sync_dir dunst
-sync_dir kitty
+sync_all() {
+  changed=0
 
+  for dir in hypr waybar wofi dunst kitty; do
+    if sync_dir "$dir"; then
+      changed=1
+    fi
+  done
+
+  if [ "$changed" -eq 0 ]; then
+    echo "No files changed!"
+  fi
+}
+
+# Initial sync
+sync_all
 echo "Install complete"
+
+# Watch mode
+if [ "$WATCH_MODE" -eq 1 ]; then
+  echo "Watching for changes..."
+  inotifywait -m -r -e modify,create,delete,move \
+    hypr waybar wofi dunst kitty |
+  while read -r _; do
+    sync_all
+  done
+fi
